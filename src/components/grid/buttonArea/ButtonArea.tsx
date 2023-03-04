e0c9acff745cc52935b7d1c39739972f108fe27a
Alex Richards <alexchristopherrichards@gmail.com>
2023-03-04 01:11:52 -0500

    correct grid display in console

---
+++ src/components/grid/buttonArea/ButtonArea.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './ButtonArea.module.scss';
+
+interface ButtonAreaProps {
+  rows: number;
+  columns: number;
+  grid: number[][];
+  setGrid: (grid: number[][]) => void;
+}
+
+export default function ButtonArea(props: ButtonAreaProps) {
+  const newGrid: number[][] = Array.from({ length: props.rows }, () =>
+    new Array(props.columns).fill(0)
+  );
+
+  const handleSubmit = () => {
+    console.log(props.grid, 'SUBMITTED RESULT');
+  };
+  return (
+    <div className={styles.buttonArea}>
+      <button onClick={() => props.setGrid(newGrid)} className={styles.reset}>
+        Reset Grid
+      </button>
+      <button onClick={() => handleSubmit()} className={styles.submit}>
+        Submit
+      </button>
+    </div>
+  );
+}
---
+++ src/functions/sigmoid.ts
@@ -0,0 +1,1 @@
+export const SIGMOID = (x: number) => 1 / (1 + Math.exp(-x));
