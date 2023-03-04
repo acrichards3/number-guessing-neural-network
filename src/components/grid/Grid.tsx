fd3914b16d48ac4fa708014f6e84681c988184af
Alex Richards <alexchristopherrichards@gmail.com>
2023-03-03 23:19:02 -0500

    drawing + reset button

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -8,26 +8,65 @@ interface GridProps {
 }
 
 export default function Grid(props: GridProps) {
-  const initialGrid = Array.from({ length: props.rows }, () =>
-    new Array(props.columns).fill(0)
-  );
+  const [isMouseDown, setIsMouseDown] = React.useState(false);
 
-  console.log(initialGrid);
+  const [grid, setGrid] = React.useState(() => {
+    return Array.from({ length: props.rows }, () =>
+      new Array(props.columns).fill(0)
+    );
+  });
+
+  const handleCellClick = (x: number, y: number) => {
+    const newGrid = [...grid];
+    newGrid[y]![x] = 1;
+    setGrid(newGrid);
+  };
+
+  const handleCellEnter = (x: number, y: number) => {
+    if (isMouseDown) {
+      const newGrid = [...grid];
+      newGrid[y]![x] = 1;
+      setGrid(newGrid);
+    }
+  };
+
+  console.log(grid);
 
   return (
-    <div className={styles.grid}>
-      {initialGrid.map((row, rowIndex) => (
-        <div key={rowIndex} className={styles.row}>
-          {row.map((cell, colIndex) => (
-            <div
-              key={`${rowIndex}-${colIndex}`}
-              className={classNames(styles.cell, {
-                [styles.filled as string]: cell === 1,
-              })}
-            />
-          ))}
-        </div>
-      ))}
-    </div>
+    <>
+      <div className={styles.grid}>
+        {grid.map((row, x) => (
+          <div key={x} className={styles.row}>
+            {row.map((cell, y) => (
+              <div
+                key={`${x}-${y}`}
+                className={classNames(styles.cell, {
+                  [styles.filled as string]: cell === 1,
+                })}
+                onMouseDown={() => setIsMouseDown(true)}
+                onMouseUp={() => setIsMouseDown(false)}
+                onMouseEnter={() => handleCellEnter(y, x)}
+                onClick={() => handleCellClick(y, x)}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className={styles.buttonArea}>
+        <button
+          onClick={() =>
+            setGrid(() => {
+              return Array.from({ length: props.rows }, () =>
+                new Array(props.columns).fill(0)
+              );
+            })
+          }
+          className={styles.reset}
+        >
+          Reset Grid
+        </button>
+        <button className={styles.submit}>Submit</button>
+      </div>
+    </>
   );
 }
